0a1cf4fa2bd4ee9ee083cd14cf8ca5171a86b265
Tan Ren Jie <e0267395@kelaberetiv.sg>
2018-07-30 12:20:02 +0800

    added a simple blog scraping script

---
+++ src/blogscrapping.py
@@ -0,0 +1,36 @@
+'''
+Script that scrap the 'http://codedemos.com/sampleblog/' blog and output a .csv containing the headers
+Title, url, date for the respective blog posts
+'''
+
+
+import requests
+import os
+from bs4 import BeautifulSoup
+from csv import writer
+
+url = 'http://codedemos.com/sampleblog/'
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+posts = soup.find_all(class_ = 'post-preview')
+# print(posts)
+
+save_path = os.path.join(os.getcwd(), 'data','scraped_data')
+with open(os.path.join(save_path, 'posts.csv'),'w') as csv_file:
+    csv_writer = writer(csv_file)
+    headers = ['Title', 'Link', 'Date']
+    csv_writer.writerow(headers)
+
+    for post in posts:
+        title = post.find(class_ = 'post-title').get_text().replace('\n', '')
+        # print(title)
+        link = post.find('a')['href']
+        # print(title, link)
+        date = post.select('.post-date')[0].get_text()
+        # print(title, link, date)
+        csv_writer.writerow([title, link, date])
+
+if __name__ =='__main__':
+    main()
---
+++ src/bs_tutorial.py
@@ -0,0 +1,68 @@
+'''
+This script would show the basics of using BeautifulSoup
+
+documentations: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+video_ref: https://www.youtube.com/watch?v=4UcqECQe5Kc&t=564s
+testing_url: http://codedemos.com/sampleblog/ (if you have wget on bash, you can wget the index.html)
+
+credit: Traversy Media
+'''
+
+from bs4 import BeautifulSoup
+import os
+
+data_path = os.path.join(os.getcwd(), 'raw','data')
+html_doc = open(os.path.join(data_path,os.listdir(data_path)[0]), 'r')
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+'''
+Direct Method
+'''
+# print(soup.body)
+# print(soup.head)
+# print(soup.head.title)
+
+'''
+find()/ find_all()
+
+Usually you wont be using direct methods
+You would use the find/ findall methods
+'''
+# el = soup.find('div')
+# el = soup.find_all('div')
+# el = soup.find(id='section-1')
+# el = soup.find(class_ = 'navbar-nav ml-auto')
+# el = soup.find(attrs = {"data-hello": "hi"})
+
+'''
+select()
+
+We also have select
+Select allows us to select things by css selectors which is very similar to jquery
+'''
+# el = soup.select('#section-1')
+# el = soup.select('.nav-item')
+
+
+'''
+get_text()
+
+Usually we just want the text inside the tags
+'''
+
+# el = soup.find(class_ = 'navbar-nav ml-auto').get_text()
+
+# for item in soup.select('.nav-item'):
+#     print(item.get_text())
+
+
+'''
+Navigation
+'''
+
+# el = soup.body.contents[7].contents[1].find_next_sibling()
+# el = soup.find(class_ = 'navbar-nav ml-auto').find_previous_sibling()
+# el = soup.find(class_ = 'nav-item').find_parent()
+el = soup.find_all('p')
+print(el)
